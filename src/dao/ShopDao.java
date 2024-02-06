05f2c6580ce6447c2538e8812f501115631f6691
nittan <nitin01kumar14@gmail.com>
2024-02-06 10:33:31 +0530

    Second Commit

--- src/dao/ShopDao.java
+++ src/dao/ShopDao.java
@@ -1,5 +1,32 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Connection.JDBC;
+import model.ShopBean;
+
 public class ShopDao {
+private Connection connection;
+public ShopDao(){
+    this.connection = JDBC.getConnection();
+}
+public ShopBean logInStaff (int id, String password) throws SQLException {
+    ShopBean result = null;
+    PreparedStatement preparedStatement = this.connection.prepareStatement("select * from shop where shop_id = ? AND shop_password = ?)");
+    preparedStatement.setInt(1,id);
+    preparedStatement.setString(2,password);
+    ResultSet resultSet = preparedStatement.executeQuery();
+    if(resultSet.next()){
+        int i = resultSet.getInt("shop_id");
+        String n = resultSet.getString("shop_name");
+        String c = resultSet.getString("shop_contact");
+        String a = resultSet.getString("shop_address");
+        result = new ShopBean(i,n,a,c);
+    }
+    return result;
+}
 
 }
--- src/model/ShopBean.java
+++ src/model/ShopBean.java
@@ -1,5 +1,63 @@
 package model;
 
 public class ShopBean {
+    public int shop_id;
+    public String shop_name;
+    public String shop_address;
+    public String shop_contact;
 
+    public int getShop_id() {
+        return shop_id;
+    }
+
+    public String getShop_name() {
+        return shop_name;
+    }
+
+    public String getShop_address() {
+        return shop_address;
+    }
+
+    public String getShop_contact() {
+        return shop_contact;
+    }
+
+    public void setShop_id(int shop_id) {
+        this.shop_id = shop_id;
+    }
+
+    public void setShop_name(String shop_name) {
+        this.shop_name = shop_name;
+    }
+
+    public void setShop_address(String shop_address) {
+        this.shop_address = shop_address;
+    }
+
+    public void setShop_contact(String shop_contact) {
+        this.shop_contact = shop_contact;
+    }
+
+    public ShopBean(int shop_id, String shop_name, String shop_address, String shop_contact) {
+        super();
+        this.shop_id = shop_id;
+        this.shop_name = shop_name;
+        this.shop_address = shop_address;
+        this.shop_contact = shop_contact;
+    }
+
+    public ShopBean() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+
+        return "ShopBean{" +
+                "shop_id=" + shop_id +
+                ", shop_name='" + shop_name + '\'' +
+                ", shop_address='" + shop_address + '\'' +
+                ", shop_contact=" + shop_contact +
+                '}';
+    }
 }
